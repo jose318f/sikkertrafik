2f4445c46db81f69e452e2b5faeff996f11dec9e
jose318f <jose318f@stud.kea.dk>
2019-03-26 17:18:13 +0100

    video og burgermenu

--- script.js
+++ script.js
@@ -15,3 +15,45 @@
 //  console.log(e.target.getAttribute("data-value"));
 
 //})
+document.addEventListener("DOMContentLoaded", start);
+
+function start() {
+    console.log("SidenVises");
+
+    document.querySelector(".search").addEventListener("click", skiftPic);
+}
+
+function skiftPic() {
+    console.log("SkiftBillede")
+
+    document.querySelector("#map").classList.add("hide");
+
+    document.querySelector("#map2").classList.remove("hide");
+}
+
+window.addEventListener("load", sidenVises);
+
+
+
+function sidenVises() {
+    console.log("siden vises");
+
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+}
+
+function toggleMenu() {
+    console.log("Toggle menu");
+    document.querySelector(".menu").classList.toggle("hidden");
+
+    let erSkjult =
+        document.querySelector(".menu").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        //menu er nu skjult - ændre menu til burger
+        document.querySelector("#menuknap").textContent = "☰";
+    } else {
+        //menu er nu vist - ændre menu til x
+        document.querySelector("#menuknap").textContent = "✖";
+    }
+
+}
